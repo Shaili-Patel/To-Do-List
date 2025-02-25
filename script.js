421072186ffa3be2bf09d7d00aee9483121e2aba
Shaili-Patel <526shailipatel@frhsd.com>
2025-02-25 09:29:37 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Initiate / Create an epmty array element called tasks
+let tasks = []
+
+// Add an ON CLICK event listener to the "add task button" that calls a function
+
+document.getElementById('addTaskBtn').addEventListener('click', function (){
+// Get the value of the input box and store it in a variable called taskInput
+let taskInput = document.getElementById('taskInput').value
+
+//Check if taskInput has something in it
+if(taskInput)
+})
